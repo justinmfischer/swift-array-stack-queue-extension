7a5b6b132dcfee6de8d2ebe0da13d05049b5d51f
Justin M Fischer <justinmfischer@gmail.com>
2014-10-21 16:59:23 -0700

    Create ArrayStackQueueExtensions.swift
    Added.

---
+++ ArrayStackQueueExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  Swift - ArrayStackQueueExtensions.swift
+//
+//  Created by JUSTIN M FISCHER on 10/21/14.
+//  Copyright (c) 2014 Fun Touch Apps, LLC. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    //Stack - LIFO
+    mutating func push(newElement: T) {
+        self.append(newElement)
+    }
+    
+    mutating func pop() -> T? {
+        return self.removeLast()
+    }
+    
+    //Queue - FIFO
+    mutating func enqueue(newElement: T) {
+        self.append(newElement)
+    }
+    
+    mutating func dequeue() -> T? {
+        return self.removeAtIndex(0)
+    }
+    
+    //Common
+    func peek() -> T? {
+        return self.first
+    }
+}
